a6d632ac69f5d079c3341b5876233efde40d94bf
Shakil <khaldishakil@gmail.com>
2025-09-17 11:21:37 +0200

    task22fix

--- wasted.go
+++ wasted.go
@@ -4,11 +4,7 @@ func Wasted(c *Character) {
 	if c.PV <= 0 {
 		println("💀 Vous êtes K.O. ! Nelson crie 'HA-HA !' au loin...")
 		c.PV = c.PVmax / 2
-<<<<<<< HEAD
-		println("🏥 Le Dr Hibbert vous soigne avec", c.PV, "PV. 'Ah-heh-heh-heh !'")
-=======
 		println("Vous avez ete ressuscite avec", c.PV, "PV.")
 		Menu(*c)
->>>>>>> 7719d8322be9b313628e15d58f0bfb17f756d403
 	}
 }
